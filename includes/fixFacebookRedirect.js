0bccb87802102dab256ec92fca165e477f9811bc
Sanchit Karve <write2sanchit@gmail.com>
2012-05-07 00:57:39 -0700

    initial commit

---
+++ includes/fixFacebookRedirect.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name           Remove Google Redirect
+// @include        https://www.facebook.com/*
+// @include        https://facebook.com/*
+// @include        http://www.facebook.com/*
+// @include        http://facebook.com/*
+// @run-at         document-start
+// based on http://userscripts.org/scripts/review/117942
+// ==/UserScript==
+
+document.addEventListener('DOMNodeInserted',checksearch,false);
+function checksearch()
+{	
+	if(window.location.hostname.match(/www\.facebook\.com/))
+	{
+		document.removeEventListener('DOMNodeInserted',checksearch,false);
+		document.addEventListener('DOMNodeInserted',huntForLinks,false);
+	}
+}
+function fixRedirect(node)
+{
+	node.removeAttribute('onmousedown');		
+	var fbRedirectURL = "http://www.facebook.com/l.php?u=";
+	var refString = /&h=(.+)$/;
+	var realHref=node.href.replace(fbRedirectURL,"");	
+	if(realHref && realHref != node.href)
+	{
+		realHref= realHref.replace(refString,"");
+		node.href=unescape(realHref);		
+	}	
+}
+
+function huntForLinks()
+{
+	function _clear(event)
+	{
+		var node=event.target;
+		if(!node || event.attrName != 'href')return;		
+		fixRedirect(node);
+	}
+	var items=document.getElementsByTagName('a');
+	for(var i=0;i<items.length;i++)
+	{
+		items[i].addEventListener('DOMAttrModified',_clear,false);
+	}
+}
